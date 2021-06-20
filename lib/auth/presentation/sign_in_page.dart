b9928a559a1e90d695aa424b1854dfe84d393824
Alex Bibiano <alex@bibiano.es>
2021-06-20 12:26:26 +0200

    sing-in page

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Icon(
+                    MdiIcons.github,
+                    size: 150.0,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  Text(
+                    'Welcome to\nRepo Viewer',
+                    style: Theme.of(context).textTheme.headline3,
+                    textAlign: TextAlign.center,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    child: const Text('Sign In'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,15 +1,50 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:resocoder_repo_viewer/auth/application/auth_notifier.dart';
+import 'package:resocoder_repo_viewer/auth/shared/provider.dart';
 import 'routes/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
   final appRouter = AppRouter();
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Resocoder Repo Viewer',
-      routerDelegate: appRouter.delegate(),
-      routeInformationParser: appRouter.defaultRouteParser(),
+  Widget build(BuildContext context, WidgetReference ref) {
+    // ref.listen<AuthState>(authNotifier, (state)) {}
+
+    return ProviderListener(
+      provider: initializationProvider,
+      onChange: (context, value) {},
+      child: ProviderListener<AuthState>(
+        provider: authNotifierProvider,
+        onChange: (context, state) {
+          state.maybeMap(
+            orElse: () {},
+            authenticated: (_) {
+              appRouter.pushAndPopUntil(
+                const StarredReposRoute(),
+                predicate: (route) => false,
+              );
+            },
+            unauthenticated: (_) {
+              appRouter.pushAndPopUntil(
+                const SignInRoute(),
+                predicate: (route) => false,
+              );
+            },
+          );
+        },
+        child: MaterialApp.router(
+          title: 'Resocoder Repo Viewer',
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+        ),
+      ),
     );
   }
 }
