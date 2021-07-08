48836d63abeca9f76d34e4e7f86a572c4250bd43
Alex Bibiano <alex@bibiano.es>
2021-07-08 10:55:35 +0200

    build runner

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,7 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:resocoder_repo_viewer/core/infrastructure/remote_response.dart';
 import 'package:resocoder_repo_viewer/github/core/infrastructure/githup_repo_dto.dart';
 
 class StarredReposRemoteService {
+  final Dio _dio;
+
   StarredReposRemoteService(this._dio);
 
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
